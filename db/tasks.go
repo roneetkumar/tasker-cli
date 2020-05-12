5c57314aa1781a159d7b6b89fa554ae2534036b3
Roneet Kumar <roneetparida@gmail.com>
2020-05-12 04:10:50 -0400

    all task

--- db/tasks.go
+++ db/tasks.go
@@ -52,6 +52,30 @@ func CreateTask(task string) (int, error) {
 
 }
 
+// AllTasks func
+func AllTasks() ([]Task, error) {
+	var tasks []Task
+
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
 func itob(v int) []byte {
 	b := make([]byte, 8)
 
